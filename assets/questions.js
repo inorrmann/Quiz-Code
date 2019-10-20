4599e9cb3927a93f39b16d9fb380613c32402f18
Ingrid N <inorrmann@gmail.com>
2019-10-19 22:55:31 -0700

    questions file has been added

---
+++ assets/questions.js
@@ -0,0 +1,55 @@
+var questionsJS = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.".
+            choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal/bash", "for loops", "console"],
+        answer: "console"
+    }
+];
+
+var questionsDisney = [
+    {
+        title: "What was the name of Moana's sidekick during her journey to return the heart of Te Fiti?",
+        choices: ["Pua", "Iago", "Heihei", "Tamatoa"],
+        answer: "Heihei"
+    },
+    {
+        title: "Who is the only Disney princess who was inspired by an actual person?"
+        choices: ["Snow White", "Pocahontas", "Aurora", "Belle"],
+        answer: "Pocahontas"
+    },
+    {
+        title: "In 1929, Mickey spoke his first words. What were they?"
+        choices: ["Oh boy!", "Oh Toodles!", "Hot dogs!", "Howdy!"],
+        answer: "Hot dogs!"
+    }
+    {
+        title: "Who was the only Disney main character who didn’t talk throughout the entire film?",
+        choices: ["Dumbo", "Aurora", "Mowgli", "Snow White"],
+        answer: "Dumbo"
+    }
+    {
+        title: "In numerous Disney-Pixar films, a yellow ball with one blue stripe and a red star makes an appearance. What is this ball’s name?",
+        choices: ["Meeko Ball", "Mushu Ball", "Terk Ball", "Luxo Ball"],
+        answer: "Lyxo Ball"
+    }
+]
